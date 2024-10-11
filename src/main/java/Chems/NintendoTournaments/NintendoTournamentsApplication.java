b5873a682fb4890bb24c6e7fd5130aad561a8508
Sun/chems Zghaidi <154335653+Chems05@users.noreply.github.com>
2024-10-11 18:52:59 +0200

    Initial commit

---
+++ src/main/java/Chems/NintendoTournaments/NintendoTournamentsApplication.java
@@ -0,0 +1,13 @@
+package Chems.NintendoTournaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NintendoTournamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NintendoTournamentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Chems/NintendoTournaments/NintendoTournamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package Chems.NintendoTournaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NintendoTournamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
